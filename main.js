2265a9dd6f9cbd93352af51ebe4a8853013df0c8
smurfbren <smurfbren@gmail.com>
2017-03-30 14:14:43 +0100

    Add files via upload
    html and CSS files

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
-import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+import './views/main.html';
+import './views/contentTemplates.html';
 
-//<head>
-	//<title>MycoolApp</title>
-//<head>
+/*
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
 
-//<body>
-//	<h1>Welcome to my new cool app</h1>
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
 
-//	{{> hello}}
-//	//{{> info}}
-//</body>
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});*/
 
-//<template name="hello">
-//	<button>Click Me</button>
-//	<p>You've pressed the button {{counter}} times.</p>
-//</template>
+Template.data.helpers({
+  vehicles : function (){
+      console.log(Vehicles.find().fetch());
+      return Vehicles.find();
+  }
+});
 
+
+Template.data.events({
+  'click #delete' : function(event, instance){
+     Vehicles.remove(this._id)
+  }
+});
+
+Template.addData.events({
+  'submit #addDataForm' : function(event, instance) {
+     event.preventDefault();
+     Vehicles.insert({make:event.target.make.value,
+                      model:event.target.model.value,
+                      age:event.target.age.value,
+                      mileage:event.target.mileage.value
+     });
+  }
 });
